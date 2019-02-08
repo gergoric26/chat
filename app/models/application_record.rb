84d59f3dfb77fcffe78328222c15c19e9ae46713
YOUR NAME <gergoric_dean@yahoo.com>
2019-02-08 18:50:37 -0500

    added controller for messages. chat is working

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # include ActionController::Helpers
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "dean", password: "password")
+User.create(username: "gergoric", password: "password")
+User.create(username: "vedran", password: "password")
+User.create(username: "dayana", password: "password")
+User.create(username: "mario", password: "password")
+
+
+Message.create(body: "New messages", user: User.last)
+Message.create(body: "New messages new new", user: User.first)
+Message.create(body: "New messages 1111", user: User.last)
